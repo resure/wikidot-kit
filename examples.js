3659bcfd7558bcd30267287ce45b0bfa4532407c
Gadzhi Gadzhiev <resure@yandex-team.ru>
2016-10-24 00:38:31 +0300

    Integrate tasks queue

--- examples.js
+++ examples.js
@@ -7,12 +7,8 @@ const WikidotKit = require('.');
 // $ WIKIDOT_API_TOKEN='your_token_here' node examples.js
 
 const wk = new WikidotKit({
-	wikiName: 'scp-ru',
 	token: process.env.WIKIDOT_API_TOKEN
 });
 
 // Fetch and print all page names in selected wiki
-wk.fetchPagesList().then(console.log);
-
-// TODO Probably it's good idea to pass wiki name as argument to each method call.
-// TODO It will allow to use queue inside WikidotKit to prevent exceeding API quota.
+wk.fetchPagesList({wiki: 'scp-ru'}).then(console.log);
--- index.js
+++ index.js
@@ -3,23 +3,33 @@
 const WikidotAJAX = require('wikidot-ajax');
 const XMLRPC = require('xmlrpc');
 const Promise = require('bluebird');
+const Bottleneck = require('bottleneck');
+
+// Wikidot API rate limited to 240 req/min (one request every 250 ms), but we're
+// adding here some additional delay just in case and considering concurrency
+const DEFAULT_DELAY = 800;
+const DEFAULT_CONCURRENCY = 3;
 
 class WikidotKit {
 	/**
 	 * @param {Object} args
 	 * @param {String} args.wikiName
 	 * @param {String} [args.user]
+	 * @param {Number} [args.concurrency]
+	 * @param {Number} [args.delay]
 	 * @param {String} args.token
 	 */
 	constructor(args) {
-		if (!args.wikiName) {
-			throw new Error('wikiName is required');
-		}
 		if (!args.token) {
 			throw new Error('token is required');
 		}
 
-		this.wikiName = args.wikiName;
+		this.concurrency = args.concurrency || DEFAULT_CONCURRENCY;
+		this.delay = args.delay || DEFAULT_DELAY;
+
+		this.limiter = new Bottleneck();
+		this.limiter.changeReservoir(this.concurrency);
+
 		this.xmlrpc = XMLRPC.createSecureClient({
 			host: 'www.wikidot.com',
 			port: 443,
@@ -32,29 +42,55 @@ class WikidotKit {
 	}
 
 	/**
+	 * Enqueues function using limiter
+	 *
+	 * @private
+	 *
+	 * @param {Function} fn
+	 * @param {Array} args
+	 *
+	 * @returns {Promise}
+	 */
+	enqueue(fn, ...args) {
+		return this.limiter.schedule(fn, ...args).finally(() => {
+			this.limiter.incrementReservoir(1);
+		});
+	}
+
+	/**
 	 * XMLRPC API method call
 	 *
+	 * @private
+	 *
+	 * @param {String} wiki
 	 * @param {String} method
 	 * @param {Object} [args]
 	 *
 	 * @returns {Promise}
 	 */
-	call(method, args) {
-		args = Object.assign({}, args, {site: this.wikiName});
-		return new Promise((resolve, reject) => {
+	call({wiki, method, args}) {
+		args = Object.assign({}, args, {site: wiki});
 
-			this.xmlrpc.methodCall(method, [args], (error, result) => {
-				if (error) {
-					reject(error);
-				} else {
-					resolve(result);
-				}
+		return this.enqueue(() => {
+			return new Promise((resolve, reject) => {
+				this.xmlrpc.methodCall(method, [args], (error, result) => {
+					if (error) {
+						reject(error);
+					} else {
+						resolve(result);
+					}
+				});
 			});
 		});
 	}
 
-	fetchPagesList() {
-		return this.call('pages.select');
+	/**
+	 * @param {String} wiki
+	 *
+	 * @returns {Promise}
+	 */
+	fetchPagesList({wiki}) {
+		return this.call({wiki, method: 'pages.select'});
 	}
 }
 
