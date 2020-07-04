8e4f33d8c8d685e89245279770f7e779918704e7
Gadzhi Gadzhiev <168296+resure@users.noreply.github.com>
2020-07-04 17:18:37 +0000

    Add repl

---
+++ src/repl.ts
@@ -0,0 +1,19 @@
+import WikidotKit from '.';
+import repl = require('repl');
+
+
+const wk = new WikidotKit(process.env.WIKIDOT_API_TOKEN as string, console.log);
+process.env.REPL_MODE = '1';
+const replServer = repl.start();
+
+Object.assign(replServer.context, {
+    r: (p: Promise<unknown>) => {
+        p.then((result) => console.log('\n\n', result, '\n\nPress enter to continue')).catch(console.error);
+    },
+
+    s: (p: Promise<unknown>) => {
+        p.then(() => console.log('\n\nDone', '\n\nPress enter to continue')).catch(console.error);
+    },
+
+    wk,
+});
